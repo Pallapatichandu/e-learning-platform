b7be5f5bf1841733ddf127ac96b69a3fa5518000
Pallapatichandu <pallapatichandu41@gmail.com>
2025-07-11 01:15:55 +0530

    fix: correct start path to js/server.js

--- js/server.js
+++ js/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // root path
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
