0fcbb209337e1729addbce514262eefb5f95e3b3
Pallapatichandu <pallapatichandu41@gmail.com>
2025-07-11 00:50:52 +0530

    fix: add js/server.js to run json-server on Render

--- js/server.js
+++ js/server.js
@@ -1,13 +1,11 @@
+// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use('/api', router);
-
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+server.use(router);
+server.listen(process.env.PORT || 8000, () => {
+  console.log('JSON Server is running');
 });
